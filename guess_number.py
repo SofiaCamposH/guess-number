6ca907cd5c1710e1e03d2d1d721791647a4760bb
María Fernanda Rodríguez <utm22030609@utma.edu.mx>
2023-06-07 11:46:29 -0600

    feat : Each user's attempt is subtracted 10 points from their initial points (100), if the points are finished it enters another cycle and in this it shows a message and the correct answer.

--- guess_number.py
+++ guess_number.py
@@ -20,3 +20,15 @@ def random_number():
             print("Congratulations you have discovered the number : ")
             print("Game over")
             break
+        else:#If the if cycle is not fulfilled, they continue 
+        #with the else and this is repeated until if is fulfilled, 
+        #all this taking into account that we still have points.
+            points -= 10
+            print("Sorry, your answer is incorrect, please try again.  ")
+            print("Your performance points are: ",points)
+          #if it runs out of points, it shows you a message and the correct answer.
+        if points == 0:
+         print("Your opportunities have been exhausted. We are sorry.  The number to guess is wrong:", numer)
+         print("Game over ")
+
+random_number()
