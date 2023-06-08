8690bd1f8e3c9795cd0e20e49cc619dcf6c27c1c
Sofia Campos <sfabilene@gmail.com>
2023-06-07 21:14:41 -0500

    feat: ciclye

--- multiplayer_guess_number.py
+++ multiplayer_guess_number.py
@@ -1,4 +1,9 @@
 from getpass import getpass
 player1points=100
-player2= getpass("write random number: ")
-
+player2= getpass("write random number: ") #getpass to keep the number hidden in the console
+#cycle for the player to guess the number
+while True:
+   player1= input("Try to guess the number: ")
+   if player1 == player2:
+    print("Congratulations,did you guess the number, you have: ",(player1points))
+    break
